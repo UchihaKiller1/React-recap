9811c7e04225000c7b1a8d81d7468b1fc3ff3e35
UchihaKiller1 <amindurajamuni@gmail.com>
2025-07-05 15:54:17 +0530

    commit 2

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -2,19 +2,39 @@ import { useState } from "react";
 
 export default function Testing(){
 
-    const[count,setCount]= useState(0)
+    const[count,setCount]= useState(0);
+    const[fruit, setFruit] =useState("Coconut");
 
     return(
 
-        <div className="w-full h-screen bg-amber-100">
-        <h1>{count}</h1>
-        <button className="bg-amber-50 m-1" onClick={()=>{
+        <div className="w-full h-screen bg-amber-100 flex flex-col justify-center items-center">
+        <h1 className="text-8xl">{count} {fruit}s</h1>
+        <button className="bg-black m-1 w-[150px] h-[50px] text-white rounded-2xl m-5 text-lg" onClick={()=>{
 
            const newCount = count+1;
            setCount(newCount);
             console.log("Clicked!");
 
         }}>Click</button>
+        <div className=" w-full h-[100px] flex justify-center items-center">
+
+            <button className="bg-amber-400 m-1 w-[150px] h-[50px] text-white rounded-2xl m-5 text-lg" 
+            onClick={()=>{
+
+                setFruit("Apple");
+            }}>Apple</button>
+
+            <button className="bg-amber-400 m-1 w-[150px] h-[50px] text-white rounded-2xl m-5 text-lg" 
+            onClick={()=>{
+
+                setFruit("Orange")
+            }}>Orange</button>
+
+            <button className="bg-amber-400 m-1 w-[150px] h-[50px] text-white rounded-2xl m-5 text-lg" 
+            onClick={()=>{
+                setFruit("Banana")
+            }}>Banana</button>
+        </div>
         </div>
     )
 }
